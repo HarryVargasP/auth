f0f5a5741a3735962d0230e12748b86e52daeb73
HarryVargasP <69601962+HarryVargasP@users.noreply.github.com>
2020-10-27 20:53:12 -0600

    Commit segunda parte

--- src/api/auth/controller.js
+++ src/api/auth/controller.js
@@ -3,4 +3,25 @@ const AuthController = {};
 // >> Here will be the
 // endpoints for the Users.
 
+AuthController.getUser = (req, res) => {
+    let { id } = req.params;
+
+    model.findById(id, { username: 1, password: 1, __v: 1 })
+    .then((user) => res.status(200).json(user))
+    .catch((err) => res.status(400).json(err));
+};
+
+AuthController.validateUser = async (req, res) => {
+    let { username, password } = req.body;
+
+    user = await model.findOne({ username: username }).then(async (user) => {
+        const match = await user.validatePassword(password);
+        if (match) {
+          res.status(200).json({ msg: "Validated" });
+        } else {
+          res.status(401).json({ msg: "Unauthorized" });
+        }
+    });
+};
+
 module.exports = AuthController;
--- src/api/auth/index.js
+++ src/api/auth/index.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const AuthController = require('./controller');
 
 const path = '/auth';
 
@@ -8,13 +9,16 @@ const { model, schema } = require("../user/model");
 // >> Here will be the
 // definition of the routes.
 
-router.get("/", async (req, res) => {
+router.get("", async (req, res) => {
   let base_uri = req.protocol + "://" + req.hostname + path;
   res.json({
     register: `${base_uri}`,
     login: `${base_uri}/login`,
   });
 });
+
+router.get('/user/:id', AuthController.getUser);
+router.get('/validate', AuthController.validateUser);
 
 module.exports = {
   path,
